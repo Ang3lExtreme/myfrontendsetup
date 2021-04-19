6d55c76e4d102454107ef0bff94de713765c62ec
Frederico Neves <frednevesal@gmail.com>
2021-04-19 03:43:15 +0100

    q

---
+++ src/pages/index.tsx
@@ -0,0 +1,23 @@
+import Head from 'next/head'
+
+
+export default function Home() {
+
+
+
+  return (
+    <div> 
+
+      <p>
+      <Head>
+        <title>homepage</title>  
+      </Head>
+      </p>
+      <main>
+        <h1 >
+        Hello world 
+        </h1>
+      </main>
+    </div>
+  )
+}
